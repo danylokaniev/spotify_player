d7735962e458cc07c9146d216478c4a2d40256f1
Danylo Kanievskyi <danylo@code-care.pro>
2021-11-01 03:49:00 +0200

    add saved tracks title

--- src/components/TrackList.tsx
+++ src/components/TrackList.tsx
@@ -4,12 +4,14 @@ import { useSelector } from 'react-redux';
 import { RootState } from '../types/store';
 import TrackListItem from './TrackListItem';
 import SavedTracksPagination from './TracksPagination';
+import { Typography } from '@mui/material';
 
 const TrackList: FunctionComponent<Record<string, never>> = () => {
   const { tracks, currentTrack } = useSelector((state: RootState) => state.app);
 
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column', height: 1 }}>
+      <Typography sx={{padding: '12px'}}>Saved tracks: </Typography>
       {tracks.map(({ track }) => (
         <TrackListItem track={track} selected={track.id === currentTrack.track.id} key={track.id} />
       ))}
